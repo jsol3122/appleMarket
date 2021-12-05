12cd3ac23bad9f9fb2afb3268e93ba21c1ecb394
rim <uham17@naver.com>
2021-12-05 20:01:13 +0900

    햬껼

--- appleMarket/src/main/webapp/js/saleboardList.js
+++ appleMarket/src/main/webapp/js/saleboardList.js
@@ -1,286 +1,82 @@
-$(function() {
-    var $searchBtn = $('.shopping_nav .search_btn'),
-        $searchForm = $('header form');
-
-    $searchBtn.click(function() {
-        $searchForm.toggleClass('active');
-    });
-
-    $('.main_slides ul').bxSlider({
-        controls: false,
-        mode: 'vertical'
-    });
-
-    var rateForm = $('.rateform'),
-        rateBtn = rateForm.find('.vote span'),
-        rateInput = rateForm.find('input');
-
-    rateBtn.click(function() {
-        var $this = $(this);
-        var rateScore = $this.attr('data-rate');
-        rateInput.val(rateScore);
-        rateBtn.removeClass('active');
-
-        rateBtn.each(function(idx) {
-            if (idx < rateScore) {
-                $(this).addClass('active');
-            }
-        });
-    });
-
-    //상품상세 이미지 변경
-    var $productPictures = $('.product_pictures'),
-        $bigImg = $productPictures.find('.big_img'),
-        $thumbImgList = $productPictures.find('.thumb_img li');
-
-    /*
-    리스트를 클릭하면 
-    클릭한 그 요소의 active 추가한다.
-    클릭한 그 요소의 자식요소 img의 data-target속성의 값을 확인해서
-    큰 이미지의 경로를 수정해야 한다.        
-    */
-    $thumbImgList.click(function() {
-        $(this).addClass('active').siblings().removeClass('active');
-        var targetImgPath = '/appleMarket/img/ware/' + $(this).find('img').attr('data-target');
-
-        $bigImg.attr('src', targetImgPath);
-    });
-
-    //상품 수량 가격 변경하기
-    var $quantity = $('.quantity'),
-        $unitprice = $quantity.attr('data-unitprice'),
-        $qtyBtn = $quantity.find('span'),
-        $qytInput = $quantity.find('input'),
-        $tagetTotal = $('.total_price .price');
-
-    /*
-    $qtyBtn클릭하면 그 요소가 class명 plus 있다면
-    참이면 (플러스를 클릭했으면)
-    $qytInput value 기존값에서 1증가
-    거짓이면(마이너스를 클릭했으면)
-    $qytInput value 기존값에서 1차감
-    */
-    $qtyBtn.click(function() {
-        var currentCount = $qytInput.val();
-
-        if ($(this).hasClass('plus')) {
-            $qytInput.val(++currentCount);
-        } else {
-            if (currentCount > 1) {
-                $qytInput.val(--currentCount);
-            }
-        }
-        //수량*단가 변수 total에 저장하고 그걸 .price 값으로 변경되도록 한다.
-        var total = (currentCount * $unitprice).toLocaleString('en');
-
-        $tagetTotal.text(total + '$');
-
-    }); // $qtyBtn.click
-
-    //category filter
-
-   /*
-
-    $(".price_range").slider({
-        range: true,
-        min: 0,
-        max: 1000,
-        values: [80, 500],
-        create: function(event, ui) {
-            var slideVal = $('.price_range span');
-            var slideFirstVal = slideVal.filter(':first-of-type');
-            var slideLastVal = slideVal.filter(':last-of-type');
-
-            slideVal.html('<i></i>');
-
-            slideFirstVal.find('i').text($(".price_range").slider("values", 0) + '$');
-            slideLastVal.find('i').text($(".price_range").slider("values", 1) + '$');
-        },
-        slide: function(event, ui) { // 값이 바뀔때 마다 할일
-            $('#from').val(ui.values[0]);
-            $('#to').val(ui.values[1]);
-
-            var slideVal = $('.price_range span');
-            var slideFirstVal = slideVal.filter(':first-of-type');
-            var slideLastVal = slideVal.filter(':last-of-type');
-
-            slideVal.html('<i></i>');
-
-            slideFirstVal.find('i').text(ui.values[0] + '$');
-            slideLastVal.find('i').text(ui.values[1] + '$');
-        }
-    });
-    $('#from').val($(".price_range").slider("values", 0)); //초기값
-    $('#to').val($(".price_range").slider("values", 1)); //초기값
-
-    $('#from').change(function() {
-        var userValue = $(this).val();
-        $(".price_range").slider("values", 0, userValue);
-
-        var slideVal = $('.price_range span');
-        var slideFirstVal = slideVal.filter(':first-of-type');
-        slideFirstVal.html('<i></i>');
-        slideFirstVal.find('i').text(userValue + '$');
-    });
-
-    $('#to').change(function() {
-        var userValue = $(this).val();
-        $(".price_range").slider("values", 1, userValue);
-
-        var slideVal = $('.price_range span');
-        var slideLastVal = slideVal.filter(':last-of-type');
-        slideLastVal.html('<i></i>');
-        slideLastVal.find('i').text(userValue + '$');
-    }); */
-
-    /*
-    var sizeFilter = $('.size_filter input');
-    var targetList = $('.new_arrivals_list li');
-
-    sizeFilter.click(function() {
-        var targetValue = [];
-        sizeFilter.filter(':checked').each(function() {
-            targetValue.push('.' + $(this).val());
-        }); //each
-        var targetClass = targetValue.join(', ');
-
-        targetList.hide();
-        $(targetClass).fadeIn();
-
-
-    }); //sizeFilter.click --9:13--
-
-    */
-
-    // init Isotope
-    var $filters = $('.combi_filters input');
-    var filters = {};
-
-    var $grid = $('.new_arrivals_list').isotope({
-        itemSelector: '.new_arrivals_list > li'
-    });
-
-   $filters.click(function() {
-        var $button = $(this);
-
-        //get group key
-        var $buttonGroup = $button.parent('div');
-        var filterGroup = $buttonGroup.attr('data-filter-group');
-
-        // set filter for grouop
-        filters[filterGroup] = $button.val();
-        // combine filters
-        var filterValue = concatValues(filters);
-        // set filter for Isotope
-        $grid.isotope({ filter: filterValue });
-        $grid.isotope( 'updateSortData', filterValue );
-
-        if ($button.val() == '*') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(0).prop('checked', false);
-        }
-
-        if ($button.val() == '.tech') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(1).prop('checked', false);
-        }
-
-        if ($button.val() == '.furniture') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(2).prop('checked', false);
-        }
-
-        if ($button.val() == '.baby') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(3).prop('checked', false);
-        }
-
-        if ($button.val() == '.living') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(4).prop('checked', false);
-        }
-
-        if ($button.val() == '.female') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(5).prop('checked', false);
-        }
-
-        if ($button.val() == '.male') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(6).prop('checked', false);
-        }
+let isEnd = false;
+let pageNum = 1;
+    
+$(function(){
+    $(window).scroll(function(){
+        let $window = $(this);
+        let scrollTop = $window.scrollTop();
+        let windowHeight = $window.height();
+        let documentHeight = $(document).height();
         
-        if ($button.val() == '.hobby') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(7).prop('checked', false);
-        }
+        // console.log("documentHeight:" + documentHeight + " | scrollTop:" + scrollTop + " | windowHeight: " + windowHeight );
         
-        if ($button.val() == '.sports') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(8).prop('checked', false);
+        // scrollbar의 thumb가 바닥 전 30px까지 도달 하면 리스트를 가져온다.
+        if( scrollTop + windowHeight + 30 > documentHeight ){
+            pageNum++;
+            fetchList();
         }
-        
-        if ($button.val() == '.beauty') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(9).prop('checked', false);
-        }
-        
-        if ($button.val() == '.pet') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(10).prop('checked', false);
-        }
-        
-        if ($button.val() == '.book') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(11).prop('checked', false);
-        }
-        
-        if ($button.val() == '.etc') {
-            $button.parent('div').find('input').prop('checked', false);
-            $button.prop('checked', true);
-        } else {
-            $button.parent('div').find('input').eq(12).prop('checked', false);
-        }
+    })
+    fetchList();
+    
+})
 
-    }); //filter click
-
-    // flatten object by concatting values
-    function concatValues(obj) {
-        var value = '';
-        for (var list in obj) {
-            value += obj[list];
-        }
-        console.log(value);
-        return value;
+let fetchList = function(){
+    if(isEnd == true){
+        return;
     }
+    
+    // 방명록 리스트를 가져올 때 시작 번호
+    // renderList 함수에서 html 코드를 보면 <li> 태그에 data-no 속성이 있는 것을 알 수 있다.
+    // ajax에서는 data- 속성의 값을 가져오기 위해 data() 함수를 제공.
+    $.ajax({
+        url:"/appleMarket/saleboard/saleboardGetList?pg=" + pageNum ,
+        type: "post",
+        dataType: "json",
+        success: function(result){
+            // 컨트롤러에서 가져온 방명록 리스트는 result.data에 담겨오도록 했다.
+            // 남은 데이터가 5개 이하일 경우 무한 스크롤 종료
+            let length = result.list.length;
+            if( length < 5 ){
+                isEnd = true;
+            }
+            $.each(result.list, function(index, DTO){
+                renderList(false, DTO);
+            })
+        }
+    });
+}
 
+let renderList = function(mode, DTO){
+	if(DTO.location_dong == undefined){ //임시로 지역 넣어놓고 gps위치 개발하면 위치값 넣어놓기
+       DTO.location_dong = '강남구'; 
+       //DTO.location1_addr2.replace(/null/g, '');
+    }
+    
+    let category = DTO.sale_category;
 
-
-
-}); //document ready function
+    // 리스트 html을 정의
+    let html = "<li id=product_"+DTO.sale_seq+">"+
+        "<div class=list_contents>"+
+            "<div class=default onclick=location.href='/appleMarket/view/saleboard/saleboardView.jsp?sale_seq="+DTO.sale_seq+"'>"+
+                //"<img src='/appleMarket/storage/"+DTO.sale_image1+"' style=width:200px;height:180px; alt=img>"+
+                "<img src='/appleMarket/storage/"+DTO.sale_image1+"' style=width:200px;height:180px; alt=img>"+
+                "<h3>"+DTO.sale_subject+"</h3>"+
+                "<h3>"+DTO.location_dong+"</h3>"+
+                "<h4>"+DTO.sale_price+"</h4>"+
+            "</div>"+
+            "<div class=hover>"+
+                "<ul>"+
+                    "<li><a href='#' class=sprites>addcart</a></li>"+
+                "</ul>"+
+            "</div>"+
+        "</div>"+
+    "</li>";
+    
+    $(".new_arrivals_list").append(html);
+    console.log(mode)
+    
+    $('.hover a').addClass('addcart');
+    $('.new_arrivals_list>li').addClass(['col-md-3', category]);
+    
 
 }
